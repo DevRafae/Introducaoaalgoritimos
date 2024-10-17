e4e65bd7be5e98f4e8878c62d2ad2397982276f0
Rafaela Januario Silva <rafacienciacomput@gmail.com>
2024-10-17 13:54:19 -0300

    Create Exercicio1L1.c

---
+++ Exercicio1L1.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main(void){
+int r;
+printf("digite um numero inteiro: ");
+scanf("%d", &r);
+  printf("o numero digitado foi: %d\n", r);
+  printf("o antecesso do numero digitado é: %d\n", r-1);
+printf("o sucessor do numero digitado é: %d", r+1);
+return 0;}
